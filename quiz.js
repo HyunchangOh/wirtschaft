d69c2ded1ac2ce4a53bd6c485d67bb83e5c358be
HyunchangOh <hyunchang@kaist.ac.kr>
2023-05-08 19:44:07 +0200

    reverse question order

--- quiz.js
+++ quiz.js
@@ -17,8 +17,57 @@ var problems={
     Liquiditätsfalle: "unendlich zinselastische Geldnachfrage",
     Arbeitsangebot: "steigt selbstverständlich mit steigenden Reallöhnen. Die Leute bekommen mehr Geld für die gleiche Arbeit. Die Opportunitätskosten von Freizeit steigen. Dementsprechend möchten die Leute mehr arbeiten.",
     Opportunitätskosten:"Vielmehr möchte man damit den entgangenen Gewinn oder Nutzen beziffern. Dieser verlorengegangene Gewinn oder Nutzen entsteht, wenn man sich zwischen verschiedenen Alternativen entscheidet. Das Gegenteil der Opportunitätskosten sind die sogenannten Opportunitätserlöse.",
-    
+    Arbeitsnachfrage: "sinkt mit steigenden Reallöhnen. Da durch die Einstellung neuer Mitarbeiter zusätzliche Kosten entstehen, die den durch diese erwirtschafteten Ertrag nicht mehr rechtfertigen.",
+    Angebotsüberschuss: "Ist das Arbeitsangebot größer als die ARbetsnachfrage, liegt ein ???. Das Resultat ist Arbeitslosigkeit.",
+    Erwerbspersonen: "hierbei die Anzahl aller Beschäftigen und Arbeitslosen",
+    FriktionielleArbeitslosigkeit: "eine Form der Arbeitslosigkeit, die entsteht wenn eine Person vorübergehend arbeitslos ist, wei die Person ihre Stelle wechselnmöchte, gerade Abitur gemacht hat, gerade ihr Studium abgeschlossen hat, etc.",
+    SaisonaleArbeitslosigkeit: "*** entsteht in einer Volkswirtschaft, weil einige Unternehmen ihre Produkte nur yu bestimmten Zeiten produzieren oder verkaufen können.",
+    StrukturelleArbeitslosigkeit: "*** entsteht dadurch, dass Arbeitsplätze aufgrund von groben Veränderungen auf dem Markt oder neuen Technologien abgebaut werden.",
+    KonjunkturelleArbeitslosigkeit: "In jeder Volkswirtschaft gibt es konjunkturelle Schwankungen, denen alle Unternehmen unterworfen sind. Entsteht aufgrund einer Reyession am Markt ein NAchfragemangel, sind Unternehmen oft gezwungen Arbeitsplätze abbauen.",
+    Kosten_der_Arbeitslosigkeit: "Die Kosten der Arbeitslosigkeit lassen sich in einyelwirtschaftliche und gesamtwirtschaftliche Kosten beziehungsweise Konsequenezen einteilen.",
+    Einkommenseinbußen: "Zu den einzelwirtschaftlichen Kosten der Arbeitslosigkeit zählen in erster Linie ???? für die betroffenen Personen.",
+    Sozialversicherung: "Zu den gesamtwirtschaftlichen Kosten der ARbeitslosigkeit zählt primär die Belastung der ????",
+    Humankapital: "Eine weitere gesamtwirtschaftliche Konsequenz ist der zwangsläufige Abbau von ?????",
+    Ineffizienz: "Damit einher geht weiterer N?achteil von Arbeitslosigkeit - die aus dem nicht genutzten Produktionsfaktor resultierende ____",
+    Kriminalität: "Wer langfristig arbeitslos ist, beginnt zu verzweifeln und sieht unter Umständen den einzigen Ausweg in der _______",
+    Willenserklärung: "Willenäußerung mit dem Ziel, rechtlich etwas zu erreichen",
+    Rechtsgeschäft: "Wird geschlossen, wenn ein Antrag im Geschäftsbereich angenommen wird",
+    Handelsgesetzbuch: "Gesetzbuch, das den Bereich des geschäftlichen Handels betrifft",
+    Bürgerliches_Gesetzbuch: "Gesetzbuch, das unter anderem den Bereich des privaten Handels betrifft",
+    Nichtigkeit: "Rechtsgeschäft, welches abgeschlossen, aber nicht gültig ist",
+    Anfechtbarkeit: "Rechtsgeschäft, welches abgeschlossen, aber für ungültig erklärt werden kann, weil ein Teilnehmer nicht geschäftsfähig ist",
+    schwebende_Unwirksamkeit: "Zustand eines Rechtsgeschäfts, welches abgeschlossen, aber für ungültig erklärt werden soll",
+    übereinstimmende_Willenserklärungen: "Wird benötigt damit ein Rechtgeschäft zustande kommt",
+    Kaufvertrag: "Vereinbarung über einen Verkauf eines Gutes",
+    Testament: "Nicht emphfangsbedürftiges einseitiges Rechtsgeschäft",
+    Mietvertrag: "Vereinbarung über die Überlassung eines Gutes für eine Gebühr",
+    Giralgeldschöpfung: " erfolgt über die Erhöhung der Menge an Giral- oder Buchgeld (Geld auf Konten), da die Banken Geld ihrer Kunden, das auf Giro- oder Sparkonten gutgeschrieben ist, nicht im Tresor aufbewahren, sondern zum überwiegenden Teil dazu benutzen, als Kredit an andere Kunden weiterzugeben",
+    Kredit: "befristet zur Verfügung gestellter Geldwert, Betrag; Darlehen",
+    Geldverbindlichkeiten: "Zahlungspflicht (Zahlungsverpflichtung), die auf die Übertragung gesetzlicher Zahlungsmittel in bestimmter Höhe gerichtet und durch Barzahlung oder im Verrechnungsverfahren zu erfüllen ist",
+    Wirtschaftskreislauf: "ein Modell einer Volkswirtschaft, in dem die wesentlichen Tauschvorgänge als Geldströme und Güterströme (Wirtschaftsobjekte) zwischen den Wirtschaftssubjekten dargestellt werden.",
+    Geldstrom: "besteht aus dem Einkommen und Konsumausgaben der Haushalte sowie den Einnahmen und Ausgaben der Unternehmer.",
+    Güterstrom: "im ???? fließen Wirtschaftsgüter von den Unternehmen zu den Konsumenten und die Produktionsfaktoren von den privaten Haushalten zu den Unternehmen,",
+    Entgelte: "jede Art der Gegenleistung, die ein Unternehmer für seine Waren/Leistungen abzüglich der Umsatzsteuer erhält",
+    Zinsen: "der Preis für das Leihen von Geld",
+    Ausgabengleichschritt: "Jede zusätzliche Investition innerhalb des Unternehmenssektors erhöht das Einkommen eines anderen Unternehmers, womit dieser, der Einnahmen aus der Investition des ersten erhält, weiderum Investitionen tätigen kann und ein weiteres Unternehmen daraus Einkommen generieren kann - insofern, wenn die Gesamtheit der Unternehmer, also diese im _______________ Investitionen tätigen, finanzieren sich deren zusätzliche Investitionen sogar selbst und insofern entstehe den Unternehmen daraus kein Kreditbedarf.",
+    Offene_Wirtschaft: "Im Wirtschaftskreislauf der _________________ wird zu den vorhandenen Sektoren der Sektor Ausland mit hinzugenommen.",
+    Leistungsbilanzdefizit: "Wenn die Exporte die Importe übertreffen, so entsteht im Inland ein positiver Außenbeitrag, d. h., es fließt zusätzlich Geld vom Ausland ins Inland (Nettoexport). Umgekehrt liegt ein negativer Außenbeitrag vor, wenn die Exporte kleiner als die Importe sind (________________).",
+    Schuldenaufnahme: "Weist in einer Volkswirtschaft die Neuverschuldung durch Private in Bezug zu gewohnten Vorperioden eine sinkende Tendenz auf und wird der Ausgabenrückgang nicht durch Entsparen kompensiert, so kann staatliche ______________ (Inland/Ausland) ein Gleichgewicht herstellen.",
+    Eigentum: "______ einer Sache ist derjenige, dem die Sache gehört",
+    Besitzer: "Ein _________ ist derjenige, in dessen Einflussbereich sich die Sache befindet und der deshalb auf sie zugreifen kann.",
+    Besitz: "Besitz ist eine Tatsache, Eigentum dagegen ist das Recht an einer Sache.",
+    Besitzerin: "Wenn Simon ein Fahrrad gehört und er es an seine Freundin Johanna für eine Fahrradtour übers Wochenende ausleiht, dann ist Johanna am Wochenende Besitzerin des Fahrrads.",
+    EinseitigeRechtsgeschäft: "hier unterscheidet man zwischen Rechtsgeschäften, bei denen die Willeneserklärung empfangsbedürftig ist und Rechtsgeschäften bei denen die Willenserklärung nicht empfangsbedürftig ist.",
+    Empfangsbedürftig: "eine Willenserklärung, deren Wirksamkeit den Zugang beim Empfänger erfordert.",
 }
+
+const keys = Object.keys(problems);
+var new_problems = {};
+keys.forEach((key)=>{
+    new_problems[problems[key]]=key;
+})
+
+problems = new_problems;
 
 const shuffleArray = array => {
     for (let i = array.length - 1; i > 0; i--) {
